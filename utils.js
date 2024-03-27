c1ef3bc500f4876b7f357e7ab98f13ec2e5597f4
reficul109 <38768149+reficul109@users.noreply.github.com>
2024-03-27 11:56:35 -0600

    maybe

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const {ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SlashCommandB
 //Palette Utils
 const getColors = require('get-image-colors');
 getColors.paletteCount = {count: 30}
-getColors.paletteMessage = function(colors, page, usID) {return ('<@' + usID + '>, Pick a New Color!\nhttps://encycolorpedia.com/' + colors[0 + (page * 5)] .toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[1 + (page * 5)].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[2 + (page * 5)].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[3 + (page * 5)].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[4 + (page * 5)].toString().substring(1));}
+getColors.paletteMessage = function(colors, page, usID) {return '<@' + usID + '>, Pick a New Color!\nhttps://encycolorpedia.com/' + colors[0 + (page * 5)] .toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[1 + (page * 5)].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[2 + (page * 5)].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[3 + (page * 5)].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[4 + (page * 5)].toString().substring(1);}
 
 //Palette UI
 const more = new ButtonBuilder().setCustomId('+').setEmoji('➕').setStyle(ButtonStyle.Success);

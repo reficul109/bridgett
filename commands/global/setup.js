838c27e044b3dacc7be7c2274ed6a6e6bef8b0da
Vivi <38768149+reficul109@users.noreply.github.com>
2024-06-19 14:25:58 -0600

    last

--- commands/global/setup.js
+++ commands/global/setup.js
@@ -17,6 +17,6 @@ module.exports = {
       var newRole = {name: "🎨 Auto-Palette 🎨", permissions: []}
       await interaction.guild.roles.create(newRole).catch(() => {return message.replyOrFollow('I Need Permission to Create New Roles...');})
       var paletteRole = interaction.guild.roles.cache.find(role => role.name === "🎨 Auto-Palette 🎨")
-      interaction.replyOrFollow({content: 'Your Server is Set-Up!\nI Created a New Role: <@&' + paletteRole.id + '>\nPosition it Wisely, If I Create More Roles, they Will be Above This One!', files: [SlashCommandBuilder.imgNew]})
+      interaction.replyOrFollow({content: 'Your Server is Set-Up!\nI Created a New Role: <@&' + paletteRole.id + '>\nPosition it Wisely, If I Create More Roles, they Will be Above This One!', files: ['./ScreenNewRoles.png']})
     }
 }}
--- utils.js
+++ utils.js
@@ -30,7 +30,6 @@ const color2 = new ButtonBuilder().setCustomId('2').setEmoji('2️⃣').setStyle
 const color3 = new ButtonBuilder().setCustomId('3').setEmoji('3️⃣').setStyle(ButtonStyle.Primary);
 const color4 = new ButtonBuilder().setCustomId('4').setEmoji('4️⃣').setStyle(ButtonStyle.Primary);
 const color5 = new ButtonBuilder().setCustomId('5').setEmoji('5️⃣').setStyle(ButtonStyle.Primary);
-
 const colorsRow = new ActionRowBuilder().addComponents(color1, color2, color3, color4, color5);
 const selectRow = new ActionRowBuilder().addComponents(more, less, none);
 ActionRowBuilder.paletteUI = [colorsRow, selectRow];
@@ -45,6 +44,5 @@ return [warningEmbed];}
 //Warning UI
 const yeah = new ButtonBuilder().setCustomId('y').setEmoji('✔️').setStyle(ButtonStyle.Success);
 const nope = new ButtonBuilder().setCustomId('n').setEmoji('✖️').setStyle(ButtonStyle.Danger);
-
 const proceedRow = new ActionRowBuilder().addComponents(yeah, nope);
 ActionRowBuilder.proceedUi = [proceedRow];

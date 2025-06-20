810e12d63b025508aa69c09f89b591f1ebafddde
Vivi <38768149+reficul109@users.noreply.github.com>
2025-06-20 11:20:55 -0600

    joder

--- commands/global/palette.js
+++ commands/global/palette.js
@@ -50,6 +50,7 @@ module.exports = {
 
       const collector = channel.createMessageComponentCollector({time: 1800000});
       collector.on('collect', async userReply => {
+        console.log(userReply)
         if (!userReply.message || userReply.message.id != botReply.id) {return;}
         await userReply.deferUpdate();
         if (userReply.user.id != user.id) {return;}
